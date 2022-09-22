c45dc6a5f5fa4122349aae4603adfdc326b69c5b
silent-sea1119 <sandragonzales56t@gmail.com>
2022-09-22 21:47:11 +0500

    Add files via upload

---
+++ src/SingleColor.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+function SingleColor({ rgb, weight, index, hexColor }) {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  var audio = new Audio("./assets/click_sound.m4a");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+        audio.play();
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied ✔️</p>}
+    </article>
+  );
+}
+
+export default SingleColor;
